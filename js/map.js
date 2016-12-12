ae8f677fbc6a5d6c6b520c6df6c38c998c426eff
Ethiy <oussama.ennafii@ign.fr>
2016-12-12 10:26:01 +0100

    target right class id

--- js/map.js
+++ js/map.js
@@ -1,6 +1,6 @@
 var map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById('map-canvas'), {
     center: {lat:48.8471, lng:2.4382},
     zoom: 17
   });

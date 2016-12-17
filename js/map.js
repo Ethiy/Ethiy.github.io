b13142d3bb7cbb526ee8d5781a8435b8f4f5667d
ETHIY <oussama.ennafii@polytechnique.org>
2016-12-17 13:37:52 +0100

    minor fix

--- js/map.js
+++ js/map.js
@@ -4,6 +4,5 @@ function initMap() {
     center: {lat:48.8471, lng:2.4382},
     zoom: 17
   });
+  google.maps.event.addDomListener(window, "load", initialize);
 }
-
-google.maps.event.addDomListener(window, "load", initialize);

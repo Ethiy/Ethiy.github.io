1a313c355d704dd661610e39d27d39db4ef46695
Ethiy <oussama.ennafii@ign.fr>
2016-12-19 10:23:26 +0100

    add event trigger

--- js/map.js
+++ js/map.js
@@ -1,7 +1,15 @@
 var map;
+
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map-canvas'), {
-    center: {lat:48.8471, lng:2.4382},
-    zoom: 17
-  });
-}
+    var coordinates = new google.maps.LatLng (48.8471, 2.4382)
+    var options = {
+        zoom: 17,
+        center: coordinates
+    };
+    map = new google.maps.Map(
+        document.getElementById('map-canvas'), 
+        options
+        );
+};
+
+google.maps.event.trigger(map, "resize");
